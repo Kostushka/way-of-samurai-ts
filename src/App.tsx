273f3806e44bc08b7e6a1cdae1724d9576c6f46f
Kostushka <nastyatery47@yandex.ru>
2022-11-03 22:56:16 +0300

    Added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
-import Header from './components/Header';
-import Technologies from './containers/Technologies';
-
-import './App.css';
+import logo from './img/logo.png';
+import headerImg from './img/header.jpg';
+import avatarImg from './img/avatar.jpg';
+import styles from './App.module.css';
 
 function App() {
     return (
-        <div className='App'>
-            <Header />
-            <Technologies />
+        <div className={styles.container}>
+            <header className={styles.header}>
+                <img src={logo} alt='лого' />
+            </header>
+            <nav className={styles.nav}>
+                <ul>
+                    <li>
+                        <a href='#'>Профиль</a>
+                    </li>
+                    <li>
+                        <a href='#'>Сообщение</a>
+                    </li>
+                    <li>
+                        <a href='#'>Новости</a>
+                    </li>
+                    <li>
+                        <a href='#'>Музыка</a>
+                    </li>
+                    <li>
+                        <a href='#'>Настройки</a>
+                    </li>
+                </ul>
+            </nav>
+            <main className={styles.main}>
+                <img src={headerImg} alt='главная' />
+                <div className={styles.main__block}>
+                    <img src={avatarImg} alt='аватар' />
+                    профиль и описание
+                    <div>Мои посты</div>
+                    <div>Новый пост</div>
+                    <div>Пост 1</div>
+                    <div>Пост 2</div>
+                </div>
+            </main>
+            <footer className={styles.footer}>Здесь пока ничего нет</footer>
         </div>
     );
 }
